a9dbdf527ba933f86bc0ee24105beb50c3465ee9
Iraklis Dimitriadis <rakli@localhost.localdomain>
2016-12-08 19:15:39 +0100

    safe current work in progress

--- src/views/arrwidgetmockup.php
+++ src/views/arrwidgetmockup.php
@@ -35,20 +35,6 @@
 
 <body>
 <?php include("menu.php"); ?>
-
-<!--<?php
-// Get all course data and name + email of their creators from our database based
-// on the subject id given in the website URL
-include '../php/db_connect.php';
-include '../php/tools.php';
-
-$subject_id = filter_input(INPUT_GET, "id");
-$subject = $db->query("SELECT * FROM subjects WHERE id='$subject_id'")->fetchObject();
-$courses = $db->query("SELECT courses.*, users.given_name AS creator_firstname, users.family_name AS creator_lastname 
-                           FROM courses JOIN users ON courses.creator=users.email 
-                           WHERE courses.id='$subject_id'")->fetchAll();
-
-?>-->
 <header id='head' class='secondary'>
     <div class='container'>
         <div class='row'>
@@ -63,8 +49,8 @@ $courses = $db->query("SELECT courses.*, users.given_name AS creator_firstname, 
             <div class='row'>
                 <!-- Info box with data about subject -->
                 <div class='col-sm-2'>
-                    <div class='featured-box'>
-                        <div class="preview-sidebar">
+                    <div class='featured-box sidebar-container'>
+                        <div class="gridstack-sidebar">
                             <div class="grid-stack-item preview-item-margin">
                                 <div class="grid-stack-item-content">slideviewer</div>
                             </div>
@@ -83,7 +69,7 @@ $courses = $db->query("SELECT courses.*, users.given_name AS creator_firstname, 
 
                 <!-- List of all courses -->
                 <div class='col-sm-10'>
-                    <div class="grid-stack grid-stack-10" id="grid1">
+                    <div class="grid-stack grid-stack-10 grid-stack-main">
                     </div>
                 </div>
             </div>
@@ -107,47 +93,37 @@ if (filter_input(INPUT_GET, "widget") == "true") {
 <script src="../external/jqueryUI/jquery-ui.min.js"></script>
 <script src="../external/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 <script src="../external/lodash/lodash.js"></script>
+<!--<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.2.5/gridstack.min.js"></script>-->
+<!--<script src="../external/gridstack/gridstack.js"></script>-->
 <script src="../external/gridstack/gridstack.all.js"></script>
 <script src="../external/gridstack/gridstack.jQueryUI.min.js"></script>
 
 <!-- Plugin JavaScript -->
 
 <script src="../js/course-list.js"></script>
-<!--<script src="../js/lodash.js"></script>-->
-<script type="text/javascript">
+<!--<script src="../js/widget-arrangement.js"><script/>-->
+<script>
     $(function () {
+
+        $canvas = $('.grid-stack-main');
         var options = {
-            width: 6,
+            width: 10,
             float: false,
             removable: '.trash',
             removeTimeout: 100,
+            placeholderClass: 'grid-stack-placeholder',
             acceptWidgets: '.grid-stack-item'
         };
-        $('#grid1').gridstack(options);
-        var items = [
-            {x: 0, y: 0, width: 2, height: 2},
-            {x: 3, y: 1, width: 1, height: 2},
-            {x: 4, y: 1, width: 1, height: 1},
-            {x: 2, y: 3, width: 3, height: 1},
-            {x: 2, y: 5, width: 1, height: 1}
-        ];
+        $canvas.gridstack(options);
 
-        $('.grid-stack').each(function () {
-            var grid = $(this).data('gridstack');
-
-            _.each(items, function (node) {
-                grid.addWidget($('<div><div class="grid-stack-item-content" /><div/>'),
-                    node.x, node.y, node.width, node.height);
-            }, this);
-        });
-
-        $('.preview-sidebar .grid-stack-item').draggable({
+        $('.gridstack-sidebar .grid-stack-item').draggable({
             revert: 'invalid',
             handle: '.grid-stack-item-content',
             scroll: false,
             appendTo: 'body'
         });
     });
+
 </script>
 </body>
 </html>

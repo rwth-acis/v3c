1a04dd1431720c8b4e091a97d9525ef81266d369
Iraklis Dimitriadis <rakli@comtip.net>
2016-12-09 10:32:50 +0100

    Added max Widget Elements lock

--- src/views/arrwidgetmockup.php
+++ src/views/arrwidgetmockup.php
@@ -49,11 +49,14 @@
     </div>
     <div class="virtus-pw-content-container">
         <div class="row virtus-pw-content-wrapper">
-            <div class="col-sm-12 virtus-pw-slide-img-wrapper"><img class="virtus-pw-sliderviewer-img" src='https://s-media-cache-ak0.pinimg.com/564x/0d/e6/b3/0de6b34699563781365b286c45359692.jpg'></div>
+            <div class="col-sm-12 virtus-pw-slide-img-wrapper"><img class="virtus-pw-sliderviewer-img"
+                                                                    src='https://s-media-cache-ak0.pinimg.com/564x/0d/e6/b3/0de6b34699563781365b286c45359692.jpg'>
+            </div>
             <div class="col-sm-12 virtus-pw-content">
                 <div class="row">
                     <div class="col-sm-4">
-                        <span class="glyphicon glyphicon glyphicon-chevron-left slideviewer-nav-icon" aria-hidden="true"></span>
+                        <span class="glyphicon glyphicon glyphicon-chevron-left slideviewer-nav-icon"
+                              aria-hidden="true"></span>
                     </div>
                     <div class="col-sm-4">
                         <span class="slide-viewer-slideindex-style">1/20</span>
@@ -72,19 +75,24 @@
     </div>
     <div class="virtus-pw-content-container">
         <div class="row virtus-pw-content-wrapper">
-            <div class="col-sm-12 virtus-pw-slide-img-wrapper"><img class="virtus-pw-sliderviewer-img" src='http://i.imgur.com/g0I8gTi.jpg'></div>
+            <div class="col-sm-12 virtus-pw-slide-img-wrapper"><img class="virtus-pw-sliderviewer-img"
+                                                                    src='http://i.imgur.com/g0I8gTi.jpg'></div>
             <div class="col-sm-12 virtus-pw-content">
                 <div class="row">
                     <div class="col-sm-3">
                     </div>
                     <div class="col-sm-6">
-                        <span class="glyphicon glyphicon glyphicon glyphicon glyphicon-fast-backward slideviewer-nav-icon videoviewer-icons-side-padding" aria-hidden="true"></span>
-                        <span class="glyphicon glyphicon glyphicon glyphicon-play slideviewer-nav-icon videoviewer-icons-side-padding" aria-hidden="true"></span>
-                        <span class="glyphicon glyphicon glyphicon glyphicon glyphicon-fast-forward slideviewer-nav-icon videoviewer-icons-side-padding" aria-hidden="true"></span><br>
+                        <span class="glyphicon glyphicon glyphicon glyphicon glyphicon-fast-backward slideviewer-nav-icon videoviewer-icons-side-padding"
+                              aria-hidden="true"></span>
+                        <span class="glyphicon glyphicon glyphicon glyphicon-play slideviewer-nav-icon videoviewer-icons-side-padding"
+                              aria-hidden="true"></span>
+                        <span class="glyphicon glyphicon glyphicon glyphicon glyphicon-fast-forward slideviewer-nav-icon videoviewer-icons-side-padding"
+                              aria-hidden="true"></span><br>
                         <span class="videoviewer-time-style">00:30:43/01:15:00</span>
                     </div>
                     <div class="col-sm-3">
-                        <span class="glyphicon glyphicon glyphicon-fullscreen slideviewer-nav-icon" aria-hidden="true"></span>
+                        <span class="glyphicon glyphicon glyphicon-fullscreen slideviewer-nav-icon"
+                              aria-hidden="true"></span>
                     </div>
                 </div>
             </div>
@@ -97,16 +105,27 @@
     </div>
     <div class="virtus-pw-content-container">
         <div class="row virtus-pw-content-wrapper">
-            <div class="col-sm-12 virtus-pw-quizzes-img-wrapper"><img class="virtus-pw-sliderviewer-img" src='http://i.imgur.com/wkxNHN2.jpg'></div>
-            <div class="col-sm-12 quizzes-question-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ?</div>
+            <div class="col-sm-12 virtus-pw-quizzes-img-wrapper"><img class="virtus-pw-sliderviewer-img"
+                                                                      src='http://i.imgur.com/wkxNHN2.jpg'></div>
+            <div class="col-sm-12 quizzes-question-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor ?
+            </div>
             <div class="col-sm-12">
                 <div class="row">
                     <div class="col-sm-12 quizzes-answers-container">
                         <div class="row">
-                            <div class="col-sm-6 quizzes-answer-box-container"><div class="quizzes-answer-box">some answer</div></div>
-                            <div class="col-sm-6 quizzes-answer-box-container"><div class="quizzes-answer-box">The answer is b</div></div>
-                            <div class="col-sm-6 quizzes-answer-box-container"><div class="quizzes-answer-box">No that is the </div></div>
-                            <div class="col-sm-6 quizzes-answer-box-container"><div class="quizzes-answer-box">ture that</div></div>
+                            <div class="col-sm-6 quizzes-answer-box-container">
+                                <div class="quizzes-answer-box">some answer</div>
+                            </div>
+                            <div class="col-sm-6 quizzes-answer-box-container">
+                                <div class="quizzes-answer-box">The answer is b</div>
+                            </div>
+                            <div class="col-sm-6 quizzes-answer-box-container">
+                                <div class="quizzes-answer-box">No that is the</div>
+                            </div>
+                            <div class="col-sm-6 quizzes-answer-box-container">
+                                <div class="quizzes-answer-box">ture that</div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -121,9 +140,23 @@
                 <!-- Info box with data about subject -->
                 <div class='col-sm-2 virtus-margin-top-15'>
                     <div class='featured-box sidebar-container'>
-                        <div class="sidebar-title">Toolbox:</div>
+                        <div class="row">
+                            <div class="col-sm-2 lock-sidebar-icon-container virtus-pw-hide">
+                                <span class="glyphicon glyphicon glyphicon-lock locked-color-style sidbeback-lock-icon" aria-hidden="true"
+                                      data-toggle="tooltip"
+                                      data-placement="bottom"
+                                      title="The sidebar is locked, because you already filled the rolespace with the maximum amount of Widgets. Remove Widgets to be able to add new Widgets again."></span>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="sidebar-title">Toolbox:</div>
+                            </div>
+                        </div>
                         <div class="gridstack-sidebar">
 
+                        </div>
+                        <div class="row sidebar-widget-counter-container">
+                            <div class="col-sm-6 sidebar-widget-counter-text">total Widgets:</div>
+                            <div class="col-sm-1 sidebar-widget-counter-number">0/3</div>
                         </div>
                         <div class="trash">
                         </div>
@@ -169,12 +202,16 @@ if (filter_input(INPUT_GET, "widget") == "true") {
 <!--<script src="../js/widget-arrangement.js"><script/>-->
 <script>
     initWidgets = [
-        {name: 'slideviewer',prototypeName: 'prototypeSlideViewer'},{name: 'videoviewer',prototypeName: 'prototypeVideoViewer'}, {name: 'quiz',prototypeName: 'prototypeQuizzesViewer'}
+        {name: 'slideviewer', prototypeName: 'prototypeSlideViewer'}, {
+            name: 'videoviewer',
+            prototypeName: 'prototypeVideoViewer'
+        }, {name: 'quiz', prototypeName: 'prototypeQuizzesViewer'}
     ];
 
 
     $(function () {
-
+        // TODO: FIX TOOLTIP HOVERING
+        //$('[data-toggle="tooltip"]').tooltip()
         var $canvas = $('#grid1');
         var $canvasContainer = $('.gridstack-canvas-container');
 
@@ -196,20 +233,20 @@ if (filter_input(INPUT_GET, "widget") == "true") {
 
         //$canvas.cellHeight($canvas.height);
         /*var items = [
-            {x: 4, y: 1, width: 1, height: 2},
-            {x: 4, y: 1, width: 1, height: 1},
-            {x: 2, y: 3, width: 3, height: 1},
-            {x: 2, y: 5, width: 1, height: 1}
-        ];
+         {x: 4, y: 1, width: 1, height: 2},
+         {x: 4, y: 1, width: 1, height: 1},
+         {x: 2, y: 3, width: 3, height: 1},
+         {x: 2, y: 5, width: 1, height: 1}
+         ];
 
-        $('.grid-stack').each(function () {
-            var grid = $(this).data('gridstack');
+         $('.grid-stack').each(function () {
+         var grid = $(this).data('gridstack');
 
-            _.each(items, function (node) {
-                grid.addWidget($('<div><div class="grid-stack-item-content" /><div/>'),
-                    node.x, node.y, node.width, node.height)
-            }, this);
-        });*/
+         _.each(items, function (node) {
+         grid.addWidget($('<div><div class="grid-stack-item-content" /><div/>'),
+         node.x, node.y, node.width, node.height)
+         }, this);
+         });*/
         $prevItems.draggable({
             revert: 'invalid',
             handle: '.grid-stack-item-content',
@@ -220,6 +257,7 @@ if (filter_input(INPUT_GET, "widget") == "true") {
         $('.grid-stack').on('change', function (event, items) {
 
             console.log("CHANGE EVENT FIRED");
+            var cntr = 0;
             $.each(items, function (index, item) {
                 var $item = (item.el).find('.grid-stack-sidebar-item');
                 //console.log($item);
@@ -232,41 +270,54 @@ if (filter_input(INPUT_GET, "widget") == "true") {
                     $item.removeClass('grid-stack-sidebar-item');
 
 
-                    var $prototypeWidget = $('#'+initWidgets[itemIndex].prototypeName);
+                    var $prototypeWidget = $('#' + initWidgets[itemIndex].prototypeName);
                     $prototypeClone = $prototypeWidget.clone();
                     $prototypeClone.removeClass('virtus-pw-hide');
                     $item.html("");
                     $item.append($prototypeClone);
                 }
-
+                cntr++;
             });
+            $('.sidebar-widget-counter-number').text(cntr + "/3");
+            if (cntr >= 3) {
+                $('.gridstack-sidebar').addClass('disable-item locked-sidebar');
+                $('.sidebar-widget-counter-container').addClass('locked-color-style');
+                $('.lock-sidebar-icon-container').removeClass('virtus-pw-hide');
+
+            } else {
+                $('.gridstack-sidebar').removeClass('disable-item locked-sidebar');
+                $('.gridstack-sidebar').find('.sidbeback-lock-icon').remove();
+                $('.sidebar-widget-counter-container').removeClass('locked-color-style');
+                $('.lock-sidebar-icon-container').addClass('virtus-pw-hide');
+            }
+            cntr = 0;
         });
     });
-function createSidebarElement(name, index){
-    $parentEl =$('.gridstack-sidebar');
-    if(index ==0){
-        $parentEl.prepend($('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="'+index+'">'+name+'</div></div>')
-            .draggable({
-                revert: 'invalid',
-                handle: '.grid-stack-item-content',
-                scroll: false,
-                appendTo: 'body',
-            }));
-    }else{
-        //$('[data-index="'+(parseInt(index)-1)+'"]').parent().after($('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="'+index+'">'+name+'</div></div>')
-        $('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="'+index+'">'+name+'</div></div>')
+    function createSidebarElement(name, index) {
+        $parentEl = $('.gridstack-sidebar');
+        if (index == 0) {
+            $parentEl.prepend($('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="' + index + '">' + name + '</div></div>')
+                .draggable({
+                    revert: 'invalid',
+                    handle: '.grid-stack-item-content',
+                    scroll: false,
+                    appendTo: 'body',
+                }));
+        } else {
+            //$('[data-index="'+(parseInt(index)-1)+'"]').parent().after($('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="'+index+'">'+name+'</div></div>')
+            $('<div class="grid-stack-item "><div class="grid-stack-item-content grid-stack-sidebar-item" data-index="' + index + '">' + name + '</div></div>')
             //.find('.grid-stack-sidebar-item').append($('.virtus-pw-prototype'))
-            .insertAfter($parentEl.find($('[data-index="'+(parseInt(index)-1)+'"]')).parent())
-            .draggable({
-                revert: 'invalid',
-                handle: '.grid-stack-item-content',
-                scroll: false,
-                appendTo: 'body',
-            });
+                .insertAfter($parentEl.find($('[data-index="' + (parseInt(index) - 1) + '"]')).parent())
+                .draggable({
+                    revert: 'invalid',
+                    handle: '.grid-stack-item-content',
+                    scroll: false,
+                    appendTo: 'body',
+                });
+        }
+
+
     }
-
-
-}
 </script>
 </body>
 </html>

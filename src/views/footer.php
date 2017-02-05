dfec0295acf22f278490c29d3c4b0ed826800e05
Tilman Berres <milkyw4i@gmail.com>
2017-02-05 20:42:41 +0100

    Re-added usermanagement to footer. Added demodata .sql file

--- src/views/footer.php
+++ src/views/footer.php
@@ -15,6 +15,8 @@ $accessControl = new AccessControl(); ?>
                             <a href="welcome.php"><?php echo getTranslation("general:button:home", "Home");?></a> |
                             <a href="subjects.php"><?php echo getTranslation("general:button:courses", "Courses");?></a> |
                             <a href="help.php"><?php echo getTranslation("general:button:help", "Help");?></a>
+                            <?php if ($accessControl->isAdmin()) { ?> | <a href="manage_users.php">Manage
+                                Users</a> <?php } ?>
                         </p>
                     </div>
                 </div>

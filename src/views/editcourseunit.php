5670f24a45ec388da215c29a240402bf66e8bd8e
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2017-03-27 17:49:04 +0200

    remove mockup from title

--- src/views/editcourseunit.php
+++ src/views/editcourseunit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Widget arrangement mockup </title>
+    <title>Widget arrangement </title>
 
     <link rel="stylesheet" href="../external/redacted/fonts/stylesheet.css"/>
     <link rel="stylesheet" href="../external/jasny-bootstrap/dist/css/jasny-bootstrap.min.css"/>

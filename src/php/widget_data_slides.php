f3262d6c94d0cc3bea69079037f04a868b57d2c2
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2017-03-30 12:47:15 +0200

    fallback language for widget endpoint

--- src/php/widget_data_slides.php
+++ src/php/widget_data_slides.php
@@ -12,10 +12,10 @@ $conn = require '../php/db_connect.php';
 $widget_instance_url = filter_input(INPUT_GET, 'widget_instance_url');
 $lang = filter_input(INPUT_GET, 'lang');
 
-$stmt = $conn->prepare("SELECT widget_data_slides.element_id, title, link
+$stmt = $conn->prepare("SELECT widget_data_slides.element_id, title, link, lang
                         FROM widget_data_slides, course_elements
                         WHERE widget_data_slides.element_id = course_elements.id
-                          AND lang = :lang
+                          AND (lang = :lang OR lang = (SELECT default_lang FROM course_elements WHERE widget_instance_url = :widget_instance_url ))
                           AND widget_instance_url = :widget_instance_url
                         LIMIT 1");
 
@@ -23,7 +23,21 @@ $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
 $stmt->bindParam(":widget_instance_url", $widget_instance_url, PDO::PARAM_STR);
 
 if ($stmt->execute() !== FALSE) {
-    $res = $stmt->fetch();
+    $res = $stmt->fetchAll();
+    if (count($res) == 1) {
+      $res = $res[0];
+    }
+    else if (count($res) == 2 && $res[0]['lang'] == $lang) {
+      $res = $res[0];
+    }
+    else if (count($res) == 2) { // && $res[1]['lang'] == $lang
+      $res = $res[1];
+    }
+    else {
+      http_response_code(404);
+      die("Not found");
+    }
+
     echo '{"element_id": '. $res['element_id'] .', "title": "'. $res['title'] .'", "link": "'. $res['link'] .'"}';
 }
 else {

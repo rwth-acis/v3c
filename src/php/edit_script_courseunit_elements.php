25cd59dc1dcf7372a825cd4472ed070c18cf95c7
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2017-04-10 17:49:54 +0200

    started quiz database implementation

--- src/php/edit_script_courseunit_elements.php
+++ src/php/edit_script_courseunit_elements.php
@@ -1,9 +1,8 @@
 <?php
-/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
+
 if (session_status() == PHP_SESSION_NONE) {
   session_start();
 }
@@ -64,9 +63,149 @@ $storeWidgetData = array(
   'hangout' => function($conn, $element_id, $lang, $data) {
   },
   'quiz' => function($conn, $element_id, $lang, $data) {
-     // TODO
+     $query = "REPLACE INTO widget_data_quiz_lng (element_id,lang,title) VALUES (:element_id, :lang, :title)";
+     $stmt = $conn->prepare($query);
+     $stmt->bindParam(":element_id", $element_id, PDO::PARAM_INT);
+     $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+     $stmt->bindValue(":title", (isset($data['title']) ? $data['title'] : ""), PDO::PARAM_STR);
+
+     if (!$stmt->execute()) {
+       http_response_code(400);
+       print_r($stmt->errorInfo());
+       die("Error saving slides data.");
+     }
+
+    // [{"widget":{"type":"quiz","title":"dfgdfgdfgdfg","questions":{"0":{"id":"","title":"dfgdfg","answers":{"0":{"id":"","title":"dfgdfgdfg","correct":"correct"},"1":{"id":"","title":"dfgdfgdfg","correct":""}}}}},"x":"2","y":"0","width":"4","height":"4"}]:""
+
+    // STORE QUESTIONS
+
+     $current_question_ids = array();
+     $question_order = 0;
+     foreach ($data['questions'] as $question) {
+       $question_id = -1;
+       if (!isset($question['id']) || $question['id'] == "") {
+         $stmt = $conn->prepare("INSERT INTO widget_data_quiz_questions (element_id, `order`) VALUES (:element_id, :order)");
+         $stmt->bindParam(":element_id", $element_id, PDO::PARAM_INT);
+         $stmt->bindParam(":order", $question_order, PDO::PARAM_INT);
+
+         if (!$stmt->execute()) {
+           http_response_code(400);
+           print_r($stmt->errorInfo());
+           die("Error.");
+         }
+
+         $question_id = $conn->lastInsertId();
+       }
+       else {
+         $stmt = $conn->prepare("UPDATE widget_data_quiz_questions SET `order` = :order WHERE id = :id");
+         $stmt->bindParam(":id", $question['id'], PDO::PARAM_INT);
+         $stmt->bindParam(":order", $question_order, PDO::PARAM_INT);
+
+         if (!$stmt->execute()) {
+           http_response_code(400);
+           print_r($stmt->errorInfo());
+           die("Error.");
+         }
+
+         $question_id = $question['id'];
+       }
+
+       $current_question_ids[] = $question_id;
+
+       $query = "REPLACE INTO widget_data_quiz_questions_lng (question_id,lang,title) VALUES (:question_id, :lang, :question)";
+       $stmt = $conn->prepare($query);
+       $stmt->bindParam(":question_id", $question_id, PDO::PARAM_INT);
+       $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+       $stmt->bindValue(":question", (isset($question['title']) ? $question['title'] : ""), PDO::PARAM_STR);
+
+       if (!$stmt->execute()) {
+         http_response_code(400);
+         print_r($stmt->errorInfo());
+         die("Error.");
+       }
+
+       // STORE answers
+
+       $current_answer_ids = array();
+       $answer_order = 0;
+       foreach ($question['answers'] as $answer) {
+         $answer_id = -1;
+         if (!isset($answer['id']) || $answer['id'] == "") {
+           $stmt = $conn->prepare("INSERT INTO widget_data_quiz_answers (question_id, `order`, correct) VALUES (:question_id, :order, :correct)");
+           $stmt->bindParam(":question_id", $question_id, PDO::PARAM_INT);
+           $stmt->bindParam(":order", $answer_order, PDO::PARAM_INT);
+           $stmt->bindValue(":correct", $answer['correct'] == "correct", PDO::PARAM_INT);
+
+           if (!$stmt->execute()) {
+             http_response_code(400);
+             print_r($stmt->errorInfo());
+             die("Error.");
+           }
+
+           $answer_id = $conn->lastInsertId();
+         }
+         else {
+           $stmt = $conn->prepare("UPDATE widget_data_quiz_answers SET `order` = :order, correct = :correct WHERE id = :id");
+           $stmt->bindParam(":id", $answer['id'], PDO::PARAM_INT);
+           $stmt->bindParam(":order", $answer_order, PDO::PARAM_INT);
+           $stmt->bindValue(":correct", $answer['correct'] == "correct", PDO::PARAM_INT);
+
+           if (!$stmt->execute()) {
+             http_response_code(400);
+             print_r($stmt->errorInfo());
+             die("Error.");
+           }
+
+           $answer_id = $answer['id'];
+         }
+
+         $current_answer_ids[] = $answer_id;
+
+         $query = "REPLACE INTO widget_data_quiz_answers_lng (answer_id,lang,title) VALUES (:answer_id, :lang, :answer)";
+         $stmt = $conn->prepare($query);
+         $stmt->bindParam(":answer_id", $answer_id, PDO::PARAM_INT);
+         $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+         $stmt->bindValue(":answer", (isset($data['title']) ? $data['title'] : ""), PDO::PARAM_STR);
+
+         if (!$stmt->execute()) {
+           http_response_code(400);
+           print_r($stmt->errorInfo());
+           die("Error.");
+         }
+
+         $answer_order ++;
+       }
+
+       $query_str = "";
+       foreach ($current_answer_ids as $id) {
+         $query_str .= " AND id != " . $id;
+       }
+
+       $stmt = $conn->prepare("DELETE FROM widget_data_quiz_answers WHERE question_id = ". $question_id ." " . $query_str);
+       if (!$stmt->execute()) {
+         http_response_code(400);
+         print_r($stmt->errorInfo());
+         die("Error.");
+       }
+
+       // END STORE ANSWERS
+
+       $question_order ++;
+     }
+
+     $query_str = "";
+     foreach ($current_question_ids as $id) {
+       $query_str .= " AND id != " . $id;
+     }
+
+     $stmt = $conn->prepare("DELETE FROM widget_data_quiz_questions WHERE element_id = ". $element_id ." " . $query_str);
+     if (!$stmt->execute()) {
+       http_response_code(400);
+       print_r($stmt->errorInfo());
+       die("Error.");
+     }
   }
-  );
+);
 
 $loadWidgetData = array(
   'slides' => function($conn, $element_id, $lang) {
@@ -113,10 +252,64 @@ $loadWidgetData = array(
     return array();
   },
   'quiz' => function($conn, $element_id, $lang) {
-    // TODO
-    return array();
+    $stmt = $conn->prepare("SELECT * FROM widget_data_quiz_lng WHERE element_id = :element_id AND lang = :lang LIMIT 1");
+    $stmt->bindParam(":element_id", $element_id, PDO::PARAM_INT);
+    $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+    if (!$stmt->execute()) {
+      echo "Error.";
+    } else {
+      $data = $stmt->fetch();
+    }
+
+    if (is_array($data))  { // TODO select fallback language ....
+      $questions = array();
+      $stmt = $conn->prepare("SELECT * FROM widget_data_quiz_questions, widget_data_quiz_questions_lng
+                                WHERE element_id = :element_id AND widget_data_quiz_questions.id = widget_data_quiz_questions_lng.question_id AND lang = :lang ORDER BY `order`");
+      $stmt->bindParam(":element_id", $element_id, PDO::PARAM_INT);
+      $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+      if (!$stmt->execute()) {
+        echo "Error.";
+      } else {
+        $data2 = $stmt->fetchAll();
+      }
+
+      foreach ($data2 as $q) {
+        $answers = array();
+        $stmt = $conn->prepare("SELECT * FROM widget_data_quiz_answers, widget_data_quiz_answers_lng
+                                  WHERE question_id = :question_id AND widget_data_quiz_answers.id = widget_data_quiz_answers_lng.answer_id AND lang = :lang ORDER BY `order`");
+        $stmt->bindParam(":question_id", $q["id"], PDO::PARAM_INT);
+        $stmt->bindParam(":lang", $lang, PDO::PARAM_STR);
+        if (!$stmt->execute()) {
+          echo "Error.";
+        } else {
+          $data1 = $stmt->fetchAll();
+        }
+
+        foreach ($data1 as $a) {
+          $answers[$a["order"]] = array(
+            "id" => $a["id"],
+            "title" => $a["title"],
+            "correct" => ($a["correct"] ? "correct" : "")
+          );
+        }
+
+        $questions[$q["order"]] = array(
+          "id" => $q["id"],
+          "title" => $q["title"],
+          "answers" => $answers
+        );
+      }
+
+      return array(
+        "title" => $data["title"],
+        "questions" => $questions
+      );
+    }
+    else {
+      return false;
+    }
   }
-  );
+);
 
 
 //Get input data from form

df6bd9a05fc5101fb115fe9f36f698c58e9d1fdc
AlexanderNeumann <alexander.t.n@web.de>
2017-04-10 16:00:58 +0200

    Removing second language selection

--- src/views/editcourse.php
+++ src/views/editcourse.php
@@ -5,57 +5,25 @@ $course_lang = filter_input(INPUT_GET, 'lang');
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge' charset='utf8'/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Edit Your Course</title>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge' charset='utf8'/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Edit Your Course</title>
 
-    <!-- Additional styles -->
-    <link rel="stylesheet" href="../css/editcourse.css">
+  <!-- Additional styles -->
+  <link rel="stylesheet" href="../css/editcourse.css">
 
 </head>
 
 <body>
-    <?php include "menu.php"; ?>
+  <?php include "menu.php"; ?>
 
-    <script>
-        function updateQueryStringParameter(uri, key, value) {
-          var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-          var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-          if (uri.match(re)) {
-            return uri.replace(re, '$1' + key + "=" + value + '$2');
-        }
-        else {
-            return uri + separator + key + "=" + value;
-        }
-    }
-
-    $( document ).ready(function() {
-        var sel = document.getElementById('change-lang');
-        sel.onchange = function() {
-          var show = document.getElementById('change-lang');
-          window.location.href = updateQueryStringParameter(document.URL,"lang",show.value);
-      }
-  });
-
-</script>
-
-<header id='head' class='secondary'>
+  <header id='head' class='secondary'>
     <div class='container'>
-        <div class='row'>
-            <h1><?php echo getTranslation("editcourse:head:edit", "Edit Your Course");?>
-                <select class="form-control" name="change-lang" id="change-lang" style="width: 150px; float: right" >
-                    <?php
-                    $languages = getSelectableLanguages();
-                    foreach ($languages as $code => $language) {
-                        $selected = ($course_lang == $code) ? "selected" : "";
-
-                        echo "<option class='flag flag-$code' value='$code' $selected>$language</option>";
-                    }
-                    ?>
-                </select>
-            </h1>
-        </div>
+      <div class='row'>
+        <h1><?php echo getTranslation("editcourse:head:edit", "Edit Your Course");?>
+        </h1>
     </div>
+</div>
 </header>
 <?php
 // Check whether the currently logged in user is allowed to edit courses

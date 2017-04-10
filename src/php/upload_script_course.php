b78be888c1f5e13c6514c6a65d7d11584d5babd0
AlexanderNeumann <alexander.t.n@web.de>
2017-04-10 14:58:20 +0200

    Creating role space and adding link to db

--- src/php/upload_script_course.php
+++ src/php/upload_script_course.php
@@ -1,12 +1,13 @@
 <?php
 
 if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+	session_start();
 }
 
 //create database connection (needs to be done before mysql_real_escape_string)
 $conn = require '../php/db_connect.php';
 require_once '../php/tools.php';
+require_once '../php/role_api.php';
 
 //Get input data from form
 $name = filter_input(INPUT_POST, 'name');
@@ -20,15 +21,15 @@ $creator = getSingleDatabaseEntryByValue('organizations', 'id', $_SESSION['affil
 
 // Create database-entry
 $statement = $conn->prepare("INSERT INTO courses (domain, creator, default_lang)
-                             VALUES (:domain, :creator, :default_lang)");
+	VALUES (:domain, :creator, :default_lang)");
 $statement->bindParam(":default_lang", $language, PDO::PARAM_STR);
 $statement->bindParam(":domain", $domain, PDO::PARAM_STR);
 $statement->bindParam(":creator", $creator['email'], PDO::PARAM_STR);
 
 $success = $statement->execute();
 if (!$success) {
-    print_r($statement->errorInfo());
-    die("Error saving course.");
+	print_r($statement->errorInfo());
+	die("Error saving course.");
 }
 
 
@@ -36,8 +37,24 @@ $course_id = $conn->lastInsertId();
 $course_lang = $language;
 
 
+// create role space
+$api = new RoleAPI("http://virtus-vet.eu:8081/", $_SESSION['access_token']);
+$api->createSpace($course_id.strtolower(urlencode(str_replace(' ', '',$name))));
+$role_url = $course_id.strtolower(urlencode(str_replace(' ', '',$name)));
+
+$statement = $conn->prepare("UPDATE courses SET space_url= :role_url 
+	WHERE id=:id");
+$statement->bindParam(":role_url", $role_url, PDO::PARAM_STR);
+$statement->bindParam(":id", $course_id, PDO::PARAM_INT);
+$success = $statement->execute();
+if (!$success) {
+	print_r($statement->errorInfo());
+	die("Error updating role url.");
+}
+
+
 $statement = $conn->prepare("INSERT INTO courses_lng (course_id, lang, name, description, profession)
-                             VALUES (:id, :language, :name, :description, :profession)");
+	VALUES (:id, :language, :name, :description, :profession)");
 $statement->bindParam(":id", $course_id, PDO::PARAM_INT);
 $statement->bindParam(":language", $language, PDO::PARAM_STR);
 $statement->bindParam(":name", $name, PDO::PARAM_STR);
@@ -45,11 +62,13 @@ $statement->bindParam(":description", $description, PDO::PARAM_STR);
 $statement->bindParam(":profession", $profession, PDO::PARAM_STR);
 
 $success = $statement->execute();
-if (!$success) {
-    print_r($statement->errorInfo());
-    die("Error saving course.");
-}
 
+
+if (!$success) {
+
+	print_r($statement->errorInfo());
+	die("Error saving course.");
+}
 
 // After creating a course, the user is redirected to the edit page. The reason
 // for this is, that it is not possible to add models on addcourse.php. But the user

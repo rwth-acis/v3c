b81c5e92e9df2cc17d1b6d111e7d6184a0fc8692
AlexanderNeumann <alexander.t.n@web.de>
2017-04-10 14:54:29 +0200

    Fixing delete
    Delete also other information and not just from courses.

--- src/php/delete_course.php
+++ src/php/delete_course.php
@@ -1,7 +1,7 @@
 <?php
 
 if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+	session_start();
 }
 
 //create database connection (needs to be done before mysql_real_escape_string)
@@ -9,27 +9,28 @@ $conn = require '../php/db_connect.php';
 
 //Get input data from form
 $course_id = filter_input(INPUT_POST, 'course_id');
-//$course_lang = filter_input(INPUT_POST, 'course_lang');
+$course_lang = filter_input(INPUT_POST, 'course_lang');
 
 // Get subject of course to return this to JS (for correct redirect)
 $stmt = $conn->prepare("SELECT courses.domain
-                        FROM courses
-                        WHERE courses.id = :course_id
-                            AND courses.lang = :course_lang
-                        LIMIT 1");
+	FROM courses, courses_lng
+	WHERE courses.id = :course_id
+	AND courses_lng.lang = :course_lang
+	AND courses_lng.course_id = courses.id
+	GROUP BY courses.domain");
 
 $stmt->bindParam(":course_id", $course_id, PDO::PARAM_INT);
-//$stmt->bindParam(":course_lang", $course_lang, PDO::PARAM_STR);
+$stmt->bindParam(":course_lang", $course_lang, PDO::PARAM_STR);
 
 $success = $stmt->execute();
 $subject = "";
 if ($success) {
-    $subject = $stmt->fetch()[0];
+	$subject = $stmt->fetch()[0];
 }
 
 // TODO: Instead of deleting courses, allow only to deactivate them (when do you really need to completely delete a course?)
 $stmt = $conn->prepare("DELETE FROM courses
-                        WHERE courses.id = :course_id");
+	WHERE courses.id = :course_id");
 
 $stmt->bindParam(":course_id", $course_id, PDO::PARAM_INT);
 //$stmt->bindParam(":course_lang", $course_lang, PDO::PARAM_STR);
@@ -39,5 +40,5 @@ $success = $stmt->execute();
 if ($success) {
     echo $subject;  // echos get returned to JS
 } else {
-    echo "FALSE";
+	echo "FALSE";
 }

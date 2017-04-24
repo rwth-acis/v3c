a38dc50194d419d19e22449c085fe4caaccd3e80
AlexanderNeumann <alexander.t.n@web.de>
2017-04-24 13:49:17 +0200

    Redirect page for joining the space

---
+++ src/php/role_redirect.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+require_once '../config/config.php';
+require '../php/role_api.php';
+if (isset($_GET["space"])) {
+	$api = new RoleAPI("http://virtus-vet.eu:8081/", $_SESSION["access_token"]);
+	$api->joinSpace($_GET["space"]);
+	$url = 'http://virtus-vet.eu:8081/spaces/'.$_GET["space"];
+	if(isset($_GET["activity"])) $url .= '#activity='.$_GET["activity"];
+    header('Location: ' . $url);
+} else {
+    header('Location: ' . $baseUrl . '/src/views/welcome.php');
+}
+

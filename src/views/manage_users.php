1e1e638000fd72df5873e50ffd52b61585ebafe0
AlexanderNeumann <alexander.t.n@web.de>
2017-10-17 16:27:09 +0200

    Added more sort options

--- src/views/manage_users.php
+++ src/views/manage_users.php
@@ -6,6 +6,28 @@
     <title>User Management | V3C</title>
 
     <link rel='stylesheet' type='text/css' href='../css/style.css'>
+    <style>
+        .hidden{
+            visibility: hidden;
+        }
+
+        i{
+            border: solid black;
+            border-width: 0 3px 3px 0;
+            display: inline-block;
+            padding: 3px;
+            margin-left: 5px;
+        }
+    	.up {
+		    transform: rotate(-135deg);
+		    -webkit-transform: rotate(-135deg);
+		}
+
+		.down {
+		    transform: rotate(45deg);
+		    -webkit-transform: rotate(45deg);
+		}
+    </style>
 </head>
 
 <body>
--- src/views/manage_users_content.php
+++ src/views/manage_users_content.php
@@ -6,7 +6,7 @@
  * Time: 16:40
  */
 
-$user_list = $db->query("SELECT * FROM users
+$user_list = $db->query("SELECT family_name,given_name,users.email,date_updated,role,affiliation,openIdConnectSub FROM users
                                   LEFT JOIN organizations
                                   ON users.affiliation = organizations.id
                                   ORDER BY family_name")->fetchAll(); // TODO not set if no affiliation exists...
@@ -56,27 +56,36 @@ $user_update_notice = "";
                         <div class="row">
                             <input id="searchString" name="searched" type="text" class="form-control"
                                    placeholder="<?php
-                echo getTranslation("usermanagement:search:name", "Search by Name");
+                echo getTranslation("usermanagement:search", "Search by");
                 ?>"
-                                   onkeyup="filter()">
+                                   onkeyup="selectFilter();">
+                                   <select name='searchType' id="searchType" onchange="selectFilter()" style="margin-left: 3px;">
+                                       <option value="0">Name</option>
+                                       <option value="1">Email</option>
+                                       <option value="2">Last Updated</option>
+                                       <option value="4">Affliation</option>
+                                   </select><div style="display:inline;margin-left: 3px;">Found: <div id="userCount" style="display:inline;">0</div></div>
                             <br/>
+
                         </div>
                     </form>
                 </div>
 
                 <!-- List of all users -->
-                <div class='col-sm-8'>
+                <div class='col-sm-11'>
                     <div>
                         <table id="user-table" class="table table-striped table-bordered table-hover">
                             <thead>
                             <tr>
-                                <th><?php echo getTranslation("usermanagement:choose:family_name",
+                                <th onclick="sortTable(0);switchClass(this);" style="cursor: pointer; cursor: hand;"><?php echo getTranslation("usermanagement:choose:family_name",
                                         "Family Name"); ?>,
                                     <?php echo getTranslation("usermanagement:choose:given_name",
-                                        "Given Name"); ?></th>
-                                <th><?php echo getTranslation("usermanagement:choose:role", "Role"); ?></th>
-                                <th><?php echo getTranslation("usermanagment:choose:affiliation",
-                                        "Affiliation"); ?></th>
+                                        "Given Name"); ?><i class="hidden"></i></th>
+                                <th onclick="sortTable(1);switchClass(this);" style="cursor: pointer; cursor: hand;"><?php echo getTranslation("usermanagement:choose:email", "Email"); ?><i class="hidden"></i></th>
+                                <th onclick="sortTable(2);switchClass(this);" style="cursor: pointer; cursor: hand;"><?php echo getTranslation("usermanagement:choose:updated", "Last Updated"); ?><i class="hidden"></i></th>
+                                <th onclick="sortTable(3);switchClass(this);" style="cursor: pointer; cursor: hand;"><?php echo getTranslation("usermanagement:choose:role", "Role"); ?><i class="hidden"></i></th>
+                                <th onclick="sortTable(4);switchClass(this);" style="cursor: pointer; cursor: hand;"><?php echo getTranslation("usermanagment:choose:affiliation",
+                                        "Affiliation"); ?><i class="hidden"></i></th>
                                 <th></th> <!--column for submit buttons -->
                             </tr>
                             </thead>
@@ -84,9 +93,11 @@ $user_update_notice = "";
                             <?php
                             foreach ($user_list as $user) {
 
-                                echo "<tr><td>" . $user['family_name'] . ", " . $user['given_name'] . "</td>";
+                                echo "<tr'><td>" . $user['family_name'] . ", " . $user['given_name'] . "</td>";
+                                echo "<td>" . $user['email'] . "</td>";
+                                echo "<td class='lu'>" . $user['date_updated'] . "</td>";
                                 echo "<form METHOD='POST' action='../php/update_users.php' enctype='multipart/form-data'>";
-                                echo "<input type='hidden' name='sub' value='$user[openIdConnectSub]'>";
+                                echo "<input type='hidden' name='sub' value='".$user['openIdConnectSub']."'>";
                                 echo "<td>";
                                 echo "<select name='role'>";
                                 foreach ($roles as $role_id => $role) {
@@ -97,7 +108,8 @@ $user_update_notice = "";
                                 echo "</td>";
                                 echo "<td>";
                                 echo "<select name='orga'>";
-                                echo "<option value='NULL'>-</option>";
+                                $selected = ($user['affiliation'] == "") ? "selected" : "";
+                                echo "<option value='NULL' $selected>-</option>";
                                 foreach ($orgas as $orga_id => $orga) {
                                     $selected = ($user['affiliation'] == $orga_id) ? "selected" : "";
                                     echo "<option value='$orga_id' $selected>$orga</option>";
@@ -118,4 +130,84 @@ $user_update_notice = "";
     </section>
 </div>
 <!-- container -->
+<script>
+// c&p and adjusted https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("user-table");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+function switchClass(div) {
+  var subDiv = div.querySelector('i');
+  var className = subDiv.getAttribute("class");
+  if(className=="hidden"|| className=="down") {
+    $("i").attr('class', 'hidden');
+    subDiv.className = "up";
+  }
+  else{
+    $("i").attr('class', 'hidden');
+    subDiv.className = "down";
+  }
+}
+
+function selectFilter(){
+    var el = document.getElementById('searchType');
+    var i = el.options[el.selectedIndex].value;
+    filter(i);
+    var rows = $('#user-table tbody tr').length;
+    var hidden = $('#user-table tbody tr[style*="display"]').length;
+    $("#userCount").html(rows-hidden); 
+}
+var rows = $('#user-table tbody tr').length;
+$("#userCount").html(rows);
+</script>
 <script src="../js/filter-users.js"></script>

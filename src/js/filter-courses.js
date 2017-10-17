3b85775c87c4eea8112d69b4ed22038effac800b
AlexanderNeumann <alexander.t.n@web.de>
2017-10-17 16:27:58 +0200

    Added sort function

--- src/js/filter-courses.js
+++ src/js/filter-courses.js
@@ -59,4 +59,49 @@ function filter(){
 
         }
     }
+    sortTable();
+}
+
+/*
+Source: https://www.w3schools.com/howto/howto_js_sort_table.asp
+Adjusted to work with the description row... 
+*/
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("course-table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = $( ".c" );
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 0; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      //check if the two rows should switch place:
+      if (x.getElementsByTagName("a")[0].innerHTML.toLowerCase() > y.getElementsByTagName("a")[0].innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      //$(rows[i]).after($(rows[i+1]));
+      var i1 = $(rows[i+1]).attr('id');
+      var d1 = document.getElementById(i1+"-desc");
+      rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+      rows[i].parentNode.insertBefore(d1, rows[i]);
+      //rows[i].parentNode.insertBefore(rows[i+3], rows[i]);
+      switching = true;
+    }
+  }
 }
